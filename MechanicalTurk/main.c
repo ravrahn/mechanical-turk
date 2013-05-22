b0ca5f1abd9be78346142d30307c4ce954847a10
Owen Cassidy <ravrahn@Allons-y.local>
2013-05-22 17:37:09 +1000

    Initial Commit

---
+++ MechanicalTurk/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  MechanicalTurk
+//
+//  Created by Owen Cassidy on 22/05/13.
+//  Copyright (c) 2013 Owen Cassidy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
