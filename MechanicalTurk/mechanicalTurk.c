57dc994b38551bb3dd335e4c7b7182957c82d3fa
Owen Cassidy <ravrahn@ravrahn.net>
2013-05-25 17:07:51 +1000

    Now Compiles!
    Some functions are stubs, but everything compiles!

--- MechanicalTurk/mechanicalTurk.c
+++ MechanicalTurk/mechanicalTurk.c
@@ -14,47 +14,169 @@
 #define MAX_ARCS 100
 #define MAX_VERTICES 100
 
+#define TRUE 1
+#define FALSE 0
+
 typedef struct _arcs {
     arc arcs[MAX_ARCS];
     int amountOfArcs;
 } arcs;
 
 typedef struct _vertices {
-    vertex vertices[MAX_vertices];
+    vertex vertices[MAX_VERTICES];
     int amountOfvertices;
 } vertices;
 
-int chooseAction(void);
-vertex chooseVertex(void);
+static action chooseAction(Game g);
+static vertex chooseCampus(Game g);
+static vertex chooseGO8(Game g);
+static arc chooseArc(Game g);
+static int chooseSpinoff(Game g);
 
-arcs arcsAroundVertex(vertex v);
-arcs arcsAroundArc(arc a);
+static arcs arcsAroundVertex(vertex v);
+static arcs arcsAroundArc(arc a);
 
-vertices verticesAroundVertex(vertex v);
-vertices verticesAroundArc(arc a);
+static vertices verticesAroundVertex(vertex v);
+static vertices verticesAroundArc(arc a);
+
+static int verticesAreEqual(vertex a, vertex b);
+static int arcsAreEqual(arc a, arc b);
+static int regionsAreEqual(region a, region b);
+
+static vertex illegalVertex(void);
+static arc illegalArc(void);
+
+static int isLegalVertex(vertex v);
+static int isLegalArc(arc a);
+
+static arcs ownedArcs(Game g, uni me);
+static vertices ownedCampuses(Game g, uni me);
 
 action bestMove(Game g) {
     action bestMove;
     
-    bestMove.actionCode = chooseAction();
+    bestMove = chooseAction(g);
     return bestMove;
 }
 
-int chooseAction(void){
-    return PASS;
+static action chooseAction(Game g){
+    
+    action legalAction;
+    
+    if(verticesAreEqual(chooseGO8(g), illegalVertex())){
+        legalAction.actionCode = BUILD_GO8;
+        legalAction.targetVertex = chooseGO8(g);
+    } else if(verticesAreEqual(chooseCampus(g), illegalVertex())){
+        legalAction.actionCode = BUILD_CAMPUS;
+        legalAction.targetVertex = chooseCampus(g);
+    } else if(chooseSpinoff(g)){
+        legalAction.actionCode = START_SPINOFF;
+    } else if(arcsAreEqual(chooseArc(g), illegalArc())){
+        legalAction.actionCode = CREATE_ARC;
+        legalAction.targetARC = chooseArc(g);
+    } else {
+        legalAction.actionCode = PASS;
+    }
+    return legalAction;
 }
 
-vertex chooseVertex(void) {
-    vertex chosen;
-    return chosen;
+static vertex chooseGO8(Game g){
+    vertex legalVertex = illegalVertex();
+    vertex testVertex;
+    uni me = getTurnNumber(g);
+    
+    //Region Coordinates lie between -2 and 2.
+    if (getStudents(g, me, STUDENT_BPS) >= 1 &&
+        getStudents(g, me, STUDENT_BQN) >= 1 &&
+        getStudents(g, me, STUDENT_MJ)  >= 1 &&
+        getStudents(g, me, STUDENT_MTV) >= 1){
+        
+        if (ownedCampuses(g, me).amountOfvertices != 0) {
+            testVertex = ownedCampuses(g, me).vertices[0];
+        }
+        
+        
+    }
+    return legalVertex;
 }
 
-arc chooseArc(void) {
-    arc chosen;
-    return chosen;
+static vertex chooseCampus(Game g){
+    vertex legalVertex = illegalVertex();
+    vertices testVertices;
+    uni me = getTurnNumber(g);
+    
+    //Region Coordinates lie between -2 and 2.
+    if (getStudents(g, me, STUDENT_BPS) >= 1 &&
+        getStudents(g, me, STUDENT_BQN) >= 1 &&
+        getStudents(g, me, STUDENT_MJ)  >= 1 &&
+        getStudents(g, me, STUDENT_MTV) >= 1){
+        
+        arcs mArcs = ownedArcs(g, me);
+        int arcCount = 0;
+        
+        while (arcCount < mArcs.amountOfArcs && verticesAreEqual(legalVertex, illegalVertex())){
+            testVertices = verticesAroundArc(mArcs.arcs[arcCount]);
+            
+            if (isLegalVertex(testVertices.vertices[0])){
+                legalVertex = testVertices.vertices[0];
+            } else if (isLegalVertex(testVertices.vertices[1])){
+                legalVertex = testVertices.vertices[1];
+            }
+            arcCount++;
+        }
+    }
+    return legalVertex;
 }
 
-arcs arcsAroundVertex(vertex v) {
+static arc chooseArc(Game g) {
+    arc legalArc = illegalArc();
+    uni me = getTurnNumber(g);
+    arcs testArcs;
+    arcs mArcs = ownedArcs(g, me);
+    
+    //Region Coordinates lie between -2 and 2.
+    if (getStudents(g, me, STUDENT_BPS) >= 1 &&
+        getStudents(g, me, STUDENT_BQN) >= 1){
+        
+        int arcCount = 0;
+        
+        while (arcCount < mArcs.amountOfArcs &&
+               arcsAreEqual(legalArc, illegalArc())) {
+            
+            testArcs = arcsAroundArc(mArcs.arcs[arcCount]);
+            
+            if (isLegalArc(testArcs.arcs[0])){
+                legalArc = testArcs.arcs[0];
+            } else if (isLegalArc(testArcs.arcs[1])){
+                legalArc = testArcs.arcs[0];
+            } else if (isLegalArc(testArcs.arcs[2])){
+                legalArc = testArcs.arcs[0];
+            } else if (isLegalArc(testArcs.arcs[3])){
+                legalArc = testArcs.arcs[0];
+            }
+            arcCount++;
+        }
+        
+        return legalArc;
+    } else {
+        return illegalArc();
+    }
+}
+
+static int chooseSpinoff(Game g) {
+    int legalSpinoff = FALSE;
+    uni me = getTurnNumber(g);
+    
+    if (getStudents(g, me, STUDENT_MJ)  >= 1 &&
+        getStudents(g, me, STUDENT_MTV) >= 1 &&
+        getStudents(g, me, STUDENT_MMONEY) >= 1){
+        legalSpinoff = TRUE;
+    }
+    
+    return legalSpinoff;
+}
+    
+static arcs arcsAroundVertex(vertex v) {
     arc a;
     arc b;
     arc c;
@@ -78,7 +200,7 @@ arcs arcsAroundVertex(vertex v) {
     return arr;
 }
 
-arcs arcsAroundArc(arc a) {
+static arcs arcsAroundArc(arc a) {
     arcs arr;
     
     arr.amountOfArcs = 4;
@@ -86,7 +208,7 @@ arcs arcsAroundArc(arc a) {
     return arr;
 }
 
-vertices verticesAroundVertex(vertex v) {
+static vertices verticesAroundVertex(vertex v) {
     vertices arr;
     
     arr.amountOfvertices = 3;
@@ -94,7 +216,7 @@ vertices verticesAroundVertex(vertex v) {
     return arr;
 }
 
-vertices verticesAroundArc(arc a) {
+static vertices verticesAroundArc(arc a) {
     vertices arr;
     
     
@@ -103,3 +225,101 @@ vertices verticesAroundArc(arc a) {
     
     return arr;
 }
+    
+//Takes in 2 vertices, checks if they are equal.
+static int verticesAreEqual(vertex a, vertex b){
+    int stage0 = FALSE;
+    int stage1 = FALSE;
+    int stage2 = FALSE;
+    int result = FALSE;
+    
+    if(regionsAreEqual(a.region0, b.region0) ||
+       regionsAreEqual(a.region0, b.region1) ||
+       regionsAreEqual(a.region0, b.region2)) {
+        //Then one of the regions is equivalent to one of the others
+        stage0 = TRUE;
+    }
+    if(regionsAreEqual(a.region1, b.region0) ||
+       regionsAreEqual(a.region1, b.region1) ||
+       regionsAreEqual(a.region1, b.region2)) {
+        //Then one of the regions is equivalent to one of the others
+        stage1 = TRUE;
+    }
+    if(regionsAreEqual(a.region2, b.region0) ||
+       regionsAreEqual(a.region2, b.region1) ||
+       regionsAreEqual(a.region2, b.region2)) {
+        //Then one of the regions is equivalent to one of the others
+        stage2 = TRUE;
+    }
+    
+    //If all three regions are equivalent, then the vertex is equivalent
+    if(stage0 == TRUE && stage1 == TRUE && stage2 == TRUE){
+        result = TRUE;
+    } else {
+        result = FALSE;
+    }
+    
+    return result;
+}
+    
+static int arcsAreEqual(arc a, arc b){
+    int result;
+    
+    if((regionsAreEqual(a.region0, b.region0) && regionsAreEqual(a.region1, b.region1))){
+        
+        result = TRUE;
+    } else if(regionsAreEqual(a.region0, b.region1) && regionsAreEqual(a.region1, b.region0)){
+        result = TRUE;
+    } else {
+        result = FALSE;
+    }
+    
+    return result;
+}
+
+static int regionsAreEqual(region a, region b) {
+    return TRUE;
+}
+
+static vertex illegalVertex(void) {
+    vertex v;
+    v.region0.x = 4;
+    v.region0.y = 4;
+    v.region1.x = 4;
+    v.region1.y = 4;
+    v.region2.x = 4;
+    v.region2.y = 4;
+    
+    return v;
+}
+
+static arc illegalArc(void) {
+    arc a;
+    a.region0.x = 4;
+    a.region0.y = 4;
+    a.region1.x = 4;
+    a.region1.y = 4;
+    
+    return a;
+}
+
+static int isLegalVertex(vertex v) {
+    return TRUE;
+}
+
+static int isLegalArc(arc a) {
+    return TRUE;
+}
+
+static arcs ownedArcs(Game g, uni me) {
+    arcs result;
+    
+    return result;
+}
+
+static vertices ownedCampuses(Game g, uni me) {
+    vertices result;
+    
+    return result;
+}
+                    
